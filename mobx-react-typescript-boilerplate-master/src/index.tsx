460f72a518ed0264968b9e544df50217aed5005d
Jean-Jacques Dubray <jdubray@gmail.com>
2016-07-23 06:24:28 -0700

    First MobX sample

---
+++ mobx-react-typescript-boilerplate-master/src/index.tsx
@@ -0,0 +1,243 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {observable, computed} from 'mobx';
+import {observer} from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
+
+const COUNTER_MAX = 10 ;
+
+class Actions {
+
+    private _present: (data: any) => void
+
+    constructor() {
+
+    }
+
+    setPresent(present: (data: any) => void) {
+        this._present = present ;
+    }
+
+    start(data: any, present?: (data: any) => void) {
+        present = present || this._present ;
+        data.started = true ;
+        present(data) ;
+        return false ;
+    }
+
+    decrement(data: any, present?: (data: any) => void) {
+        present = present || this._present ;
+        data = data || {} ;
+        data.counter = data.counter || 10 ;
+        var d = data ;
+        var p = present ;
+        setTimeout(function() {
+            d.counter = d.counter - 1 ;
+            p(d) ;
+        }, 1000) ;
+    }
+
+    launch(data: any, present?: (data: any) => void) {
+        present = present || this._present ;
+        data.launched = true ;
+        present(data) ;
+    }
+
+    abort(data: any, present?: (data: any) => void) {
+        present = present || this._present ;
+        data.aborted = true ;
+        present(data) ;
+        return false ;
+    }
+
+
+}
+
+class State {
+    @observable _counter = COUNTER_MAX;
+    @observable _aborted = false;
+    @observable _started = false;
+    @observable _launched = false;
+
+    public actions: Actions
+
+    constructor( a: Actions) {
+        this.actions = a ;
+    }
+
+    getActions() {
+        return actions ;
+    }
+
+    representation (model) {
+        console.log(model) ;
+        this._counter = model.counter;
+        this._started = model.started;
+        this._aborted = model.aborted;
+        this._launched = model.launched;
+
+    }
+
+    // Derive the current state of the system
+    ready(model) {
+        return ((model.counter === COUNTER_MAX) && !model.started && !model.launched && !model.aborted) ;
+    }
+
+    @computed get readyState() {
+        return ((this._counter === COUNTER_MAX) && !this._started && !this._launched && !this._aborted);
+    }
+
+    counting(model) {
+        var status = ((model.counter <= COUNTER_MAX) && (model.counter >= 0) && model.started && !model.launched && !model.aborted) ;
+        return status ;
+    }
+
+    @computed get countingState() {
+        return ((this._counter <= COUNTER_MAX) && (this._counter >= 0) && this._started && !this._launched && !this._aborted) ;
+    }
+
+    launched(model) {
+        return ((model.counter == 0) && model.started && model.launched && !model.aborted) ;
+    }
+
+    @computed get launchedState() {
+        return ((this._counter == 0) && this._started && this._launched && !this._aborted) ;
+    }
+
+    aborted(model) {
+        return (
+            (  model.counter <= COUNTER_MAX) && (model.counter >= 0) 
+            && model.started && !model.launched && model.aborted ) ;
+    }
+
+    @computed get abortedState() {
+        return ( ( this._counter <= COUNTER_MAX) && (this._counter >= 0) 
+            && this._started && !this._launched && this._aborted ) ;
+    }
+
+
+
+// Next action predicate, derives whether
+// the system is in a (control) state where
+// an action needs to be invoked
+
+    nextAction(model) {
+        if (this.counting(model)) {
+            if (model.counter>0) {
+                actions.decrement({counter: model.counter},model.present) ;
+            }
+
+            if (model.counter === 0) {
+                actions.launch({},model.present) ;
+            }
+        }
+    }
+
+    render(model) {
+        this.representation(model)
+        this.nextAction(model) ;
+    }
+}
+
+class Model {
+
+    private model: any ;
+
+    constructor(private state: State) {
+        this.model = {
+            counter: COUNTER_MAX, 
+            started: false,      
+            launched: false, 
+            aborted: false
+        }
+
+        this.state.render(this.model) ;
+    }
+    
+    present(data) {        
+        if (this.state.counting(this.model)) {
+            if (this.model.counter === 0) {
+                this.model.launched = data.launched || false ;
+            } else {
+                this.model.aborted = data.aborted || false ;
+                if (data.counter !== undefined) { this.model.counter = data.counter ; }
+            }
+        } else {
+            if (this.state.ready(this.model)) {
+                this.model.started = data.started || false ;
+            }
+        }
+        this.state.render(this.model) ;
+    }
+}
+
+@observer
+class RocketLauncherView extends React.Component<{stateRepresentation: State}, {}> {
+    render() {
+        console.log(this.props.stateRepresentation);
+        if (this.props.stateRepresentation.countingState) {
+            return (
+                <div>
+                    <p>Count down:{this.props.stateRepresentation._counter}</p>
+                    <button onClick={this.abort}>
+                    Abort
+                    </button>
+                    <DevTools />
+                </div>
+            );
+        }
+        if (this.props.stateRepresentation.abortedState) {
+            return (
+                <div>
+                    <p>Aborted at: {this.props.stateRepresentation._counter} s</p>
+                    <DevTools />
+                </div>
+            );
+        }
+        if (this.props.stateRepresentation.launchedState) {
+            return (
+                <div>
+                    <p>Launched</p>
+                    <DevTools />
+                </div>
+            );
+        }
+        return (
+            <div>
+                <p>Count down:{this.props.stateRepresentation._counter}</p>
+                <button onClick={this.start}>
+                Start
+                </button>
+                <DevTools />
+            </div>
+        );
+     }
+
+     start = (event) => {
+         event.preventDefault();
+         console.log(event);
+         this.props.stateRepresentation.actions.start({});
+     }
+
+     abort = (event) => {
+         event.preventDefault();
+         console.log(event);
+         this.props.stateRepresentation.actions.abort({});
+     }
+};
+
+// Instantiating the SAM pattern
+const actions = new Actions();
+const state = new State(actions);
+const model =  new Model(state);
+
+function present(model: Model) {
+    let _model = model;
+    return function(data: any) {
+         _model.present(data) ;
+    } 
+} 
+
+actions.setPresent(present(model));
+
+ReactDOM.render(<RocketLauncherView stateRepresentation={state} />, document.getElementById('root'));
