e671f4e9b1ed82ed6a6921e2829530d23164abc2
XGEN-IO\jdubray <jdubray@gmail.com>
2019-07-13 12:43:58 -0700

    Adds missing display.js file

---
+++ todomvc-app/sam/display.js
@@ -0,0 +1,10 @@
+
+export const display = (stateRepresentation) => {
+    Object.keys(stateRepresentation).forEach(function (el) {
+        document.getElementById(el).innerHTML = stateRepresentation[el]
+    });
+
+    // clean up edited field
+    document.getElementById('new-todo').value = ''
+}
+
